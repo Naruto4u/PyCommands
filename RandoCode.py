521e5e20507463fceb98955e4e4c8dece0366815
Hapyto4y <jdani297@evit.edu>
2024-08-07 09:54:19 -0700

    Random number picker 1-10
    Don't ask

--- RandoCode.py
+++ RandoCode.py
@@ -1,3 +1,11 @@
-VariableNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-random.choice(VariableNumbers)
+import random
 
+VariableNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+RandomNumber = random.choice(VariableNumbers)
+UserInput = int(input("Please input a number here: "))
+
+if UserInput == RandomNumber:
+    print("User input matches the random number!")
+else:
+    print("User input does not match the random number. The random number was:", RandomNumber)
+#Do not ask about the gambling machine I am creating, please

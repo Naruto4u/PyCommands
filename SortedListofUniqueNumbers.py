c15de062be80dd2015b1c3c99b5d31ad4a169019
Hapyto4y <jdani297@evit.edu>
2024-08-13 10:02:27 -0700

    Create SortedListofUniqueNumbers.py

---
+++ SortedListofUniqueNumbers.py
@@ -0,0 +1,8 @@
+UserList = [int(x) for x in input("List please:").split()]
+UserList.sort()
+SecondList = []
+for x in UserList:
+    if x not in SecondList:
+        SecondList.append(x)
+
+print(len(SecondList))
